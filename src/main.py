291c09ea2766c835811f7314c31b4acb5b50ac8f
GonzaloAlcalaGD <galcala@griddynamics.com>
2023-03-28 17:19:36 -0600

    add nested list handling logic

--- src/main.py
+++ src/main.py
@@ -1,7 +1,5 @@
-import requests
+import praw
 import logging
-import re
-from requests.auth import HTTPBasicAuth
 from utils import parse_arguments as args
 
 logging.basicConfig(level=logging.INFO)
@@ -20,26 +18,49 @@ def get_tech_stack_comment(comments: list) -> list:
             DBT
     2.
     """
-    pattern: str =  r'^7\)?'
-
     tech_stacks: list = []
     all_comments: list = []
+    nested_comments: list = []
 
     # Split the comments by break line
     for comment in comments:
-        all_comments.append(comment['data']['body'].split('\n\n'))
-        
-    # Extract the tech stack from the pattern
+        all_comments.append(comment.body.split('\n\n'))
+    
+    # Some comment's are in one line separated by \n , we need to split them
     for comment in all_comments:
-        logging.info('-'*100)
-        for i, line in enumerate(comment):
-            # Finds comment type #1
-            if line.startswith('7') and i < len(comment) - 1 and comment[i+1] is not None:
-                tech_stacks.append(comment[i+1].split('\n'))
-            logging.info(line)
+        if len(comment) == 1:
+            nested_comments.append(comment[0].split('\n'))
+    
+    # Flat list of list
+    nested_comments = [line for sublist in nested_comments for line in sublist]
 
+    # We save the comments with the tech stack
+    for comment in nested_comments:
+        if comment.startswith('7'):
+            tech_stacks.append(comment)
+
+    # Extract the tech stack from the pattern
+    # for comment in all_comments:
+        # logging.info('+'*100)
+
+    # for i, line in enumerate(comment):
+    #     # print(f'Line {i+1} \n {line}')
+    #     if line.startswith('7') or line.startswith('7)'):
+    #         tech_stacks.append(comment[i:])
+    #     try:
+    #         nested_comment: list = line.split('\n\n')
+    #         for nested in enumerate(nested_comment):
+    #             if nested.startswith('7') or nested.startswith('7)'):
+    #                 tech_stacks.append(nested)
+    #     except Exception as e :
+    #         pass
+    #         # logging.info(str(e))
             
-    return
+    for tech in tech_stacks:
+        print('-'*100)
+        print(tech)
+
+
 
 def retrieve_comments(
     username: str,
@@ -71,34 +92,24 @@ def retrieve_comments(
         logging.warning("Quarterly id not provided")
         return
 
-    auth = HTTPBasicAuth(client_id, client_key)
-    data: dict = {
-        "grant_type": "password",
-        "username": username,
-        "password": password,
-    }
-    headers: dict = {"User-Agent": user_agent}
-
-    response = requests.post(
-        "https://www.reddit.com/api/v1/access_token",
-        auth=auth,
-        data=data,
-        headers=headers,
+    # Create a Reddit instance using PRAW
+    reddit = praw.Reddit(
+        client_id=client_id,
+        client_secret=client_key,
+        password=password,
+        user_agent=user_agent,
+        username=username,
     )
-    response.raise_for_status()
-    access_token = response.json()["access_token"]
 
-    # Get the comments using the Reddit API
-    headers["Authorization"] = f"bearer {access_token}"
-    response = requests.get(
-        f"https://oauth.reddit.com/comments/{thread_id}?depth=10&limit=500",
-        headers=headers,
-    )
-    response.raise_for_status()
-    data = response.json()
+     # Fetch the submission using the thread_id
+    submission = reddit.submission(id=thread_id)
 
-    # Extract and print the comments
-    comments: list = data[1]["data"]["children"]
+    
+    print(f'Successfully connected as user = {reddit.user.me()}')
+
+    # Fetch and return the comments
+    submission.comments.replace_more(limit=None)
+    comments = [comment for comment in submission.comments.list()]
 
     return comments
 
